69cf351d0a6c465fd1caf46ad0e6cbc4028737b4
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-07 08:50:15 +0700

    feat: load persistent profile photo module

--- attendance-recap-mode.js
+++ attendance-recap-mode.js
@@ -1,1 +1,8 @@
-(function(){window.__CQ_ATTENDANCE_RECAP_MODE_LEGACY_DISABLED__=true;})();
+(function(){
+  window.__CQ_ATTENDANCE_RECAP_MODE_LEGACY_DISABLED__=true;
+  if(document.querySelector('script[data-cq-profile-photo]')) return;
+  var s=document.createElement('script');
+  s.src='profile-photo.js?v=20260907-profile1';
+  s.dataset.cqProfilePhoto='1';
+  document.body.appendChild(s);
+})();
