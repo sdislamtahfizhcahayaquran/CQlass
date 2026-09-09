22c40e6ea205ed74c369e1c1d57cca90ef05d9ed
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-09 10:45:17 +0700

    Load Saturday activity selector integration in timesheet

--- teacher-timesheet.js
+++ teacher-timesheet.js
@@ -7,6 +7,7 @@
     s.src=src;
     document.head.appendChild(s);
   }
-  add('teacher-timesheet-v2.js?v=20260909-saturday3','cq-ts-v2');
-  add('admin-timesheet-master.js?v=20260909-saturday3','cq-ts-admin-master');
+  add('teacher-timesheet-v2.js?v=20260909-saturday4','cq-ts-v2');
+  add('admin-timesheet-master.js?v=20260909-saturday4','cq-ts-admin-master');
+  add('timesheet-saturday-manual.js?v=20260909-saturday4','cq-ts-saturday-manual');
 })();
