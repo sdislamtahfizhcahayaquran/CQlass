0659fc8a260a2d17ac10adf529f1141d2304f260
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-15 10:19:02 +0700

    Bust cache for Kesiswaan dated recaps

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -1,5 +1,5 @@
 (function(){
-  ['internal-report-center.js?v=20260911-sapras-dashboard1','kesiswaan-points-recap.js?v=20260915-1'].forEach(function(src){
+  ['internal-report-center.js?v=20260911-sapras-dashboard1','kesiswaan-points-recap.js?v=20260915-2'].forEach(function(src){
     var s=document.createElement('script');
     s.src=src;
     s.defer=true;
