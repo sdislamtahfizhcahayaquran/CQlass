c67b79a504356c60f1116b4bfa416a175ae27aba
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-15 14:46:33 +0700

    Load Admin to Kabid Kegiatan raw extracurricular workflow

--- attendance-recap-mode.js
+++ attendance-recap-mode.js
@@ -1,1 +1,8 @@
-(function(){window.__CQ_ATTENDANCE_RECAP_MODE_LEGACY_DISABLED__=true;})();
+(function(){
+  window.__CQ_ATTENDANCE_RECAP_MODE_LEGACY_DISABLED__=true;
+  if(document.querySelector('script[data-cq-raw-exkul]')) return;
+  var s=document.createElement('script');
+  s.src='extracurricular-raw-ui.js?v=20260915-raw1';
+  s.dataset.cqRawEkskul='1';
+  document.head.appendChild(s);
+})();
