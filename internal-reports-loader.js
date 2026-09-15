311323e7f3a687ded2bfda7f1f5636f081927181
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-15 10:12:55 +0700

    Load Kesiswaan discipline and reward recap UI

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -1,1 +1,8 @@
-(function(){var s=document.createElement('script');s.src='internal-report-center.js?v=20260911-sapras-dashboard1';s.defer=true;document.head.appendChild(s)})();
+(function(){
+  ['internal-report-center.js?v=20260911-sapras-dashboard1','kesiswaan-points-recap.js?v=20260915-1'].forEach(function(src){
+    var s=document.createElement('script');
+    s.src=src;
+    s.defer=true;
+    document.head.appendChild(s);
+  });
+})();
