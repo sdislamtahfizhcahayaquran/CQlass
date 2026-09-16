2b1926f68e93ff4de64424f1e1242c794228c9d9
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-16 12:15:59 +0700

    Load latest extracurricular manager after legacy scripts

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -1,5 +1,10 @@
 (function(){
-  ['internal-report-center.js?v=20260911-sapras-dashboard1','kesiswaan-points-recap.js?v=20260915-3'].forEach(function(src){
+  [
+    'internal-report-center.js?v=20260911-sapras-dashboard1',
+    'kesiswaan-points-recap.js?v=20260915-3',
+    'extracurricular-raw-ui.js?v=20260916-raw2',
+    'kegiatan-exkul-capacity.js?v=20260916-exkul4'
+  ].forEach(function(src){
     var s=document.createElement('script');
     s.src=src;
     s.defer=true;
