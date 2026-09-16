e3867c5cdd68fd08f4c06492898d2e9d6d1f8274
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-16 12:38:17 +0700

    Bump extracurricular group view cache version

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -3,7 +3,7 @@
     'internal-report-center.js?v=20260911-sapras-dashboard1',
     'kesiswaan-points-recap.js?v=20260915-3',
     'extracurricular-raw-ui.js?v=20260916-raw2',
-    'kegiatan-exkul-capacity.js?v=20260916-exkul4'
+    'kegiatan-exkul-capacity.js?v=20260916-exkul5'
   ].forEach(function(src){
     var s=document.createElement('script');
     s.src=src;
