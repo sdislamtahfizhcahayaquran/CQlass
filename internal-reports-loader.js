531946cd274e818a1c99bf2c4a6a5582e02fa817
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-21 16:01:53 +0700

    Force-load fixed promotion report module

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -11,6 +11,7 @@
     'kesiswaan-excel-xlsx.js?v=20260921-xlsx1',
     'student-affairs-center.js?v=20260921-center2',
     'kesiswaan-final-cleanup.js?v=20260921-final2',
+    'promotion-report.js?v=20260921-live6',
     'extracurricular-raw-ui.js?v=20260916-raw2',
     'kegiatan-exkul-capacity.js?v=20260918-extpts2',
     'kegiatan-exkul-report-grades.js?v=20260921-reportgrades2',
