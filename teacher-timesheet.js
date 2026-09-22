22f7217aae5524fe874fe39bc1d857283ea60888
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-22 12:06:59 +0700

    Load polished Timesheet UI

--- teacher-timesheet.js
+++ teacher-timesheet.js
@@ -9,6 +9,7 @@
   }
   add('teacher-timesheet-v2.js?v=20260922-saturday-workday1','cq-ts-v2');
   add('timesheet-recurring.js?v=20260922-recurring2','cq-ts-recurring');
+  add('timesheet-ui-polish.js?v=20260922-polish1','cq-ts-ui-polish');
   add('admin-timesheet-master.js?v=20260922-hrd-saturday1','cq-ts-admin-master');
   add('timesheet-saturday-manual.js?v=20260922-priority1','cq-ts-saturday-manual');
 })();
