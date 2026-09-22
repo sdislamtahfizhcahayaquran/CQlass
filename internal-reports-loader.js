d4d24770ed967cde1834037a92256caa092ff536
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-22 15:11:03 +0700

    Bump achievement stars cache version

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -38,7 +38,7 @@
     'report-preview-v2-route.js?v=20260917-report3',
     'rapor-identity-fix.js?v=20260917-nisnisn1',
     'academic-report-class-picker.js?v=20260921-classpicker1',
-    'rapor-achievement-stars.js?v=20260917-stars7',
+    'rapor-achievement-stars.js?v=20260922-stars8',
     'kesiswaan-points-recap.js?v=20260915-3',
     'kesiswaan-super-report.js?v=20260921-live2',
     'kesiswaan-case-followup-ui.js?v=20260921-followup1',
