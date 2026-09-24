6b818f0d16067134c8a5d4ad80e42f6a175b5f0c
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-25 05:28:07 +0700

    Align HRD loader with parse-fixed workspace

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -65,7 +65,7 @@
     started=true;
 
     if(isHrd()){
-      load('hrd-role-cleanup.js?v=20260925-hrd-activation5');
+      load('hrd-role-cleanup.js?v=20260925-parsefix7');
       return true;
     }
     if(r==='kabid_tahfizh')return true;
