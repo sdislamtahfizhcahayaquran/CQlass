f0897913fa3bdc6d135d67b04bbfb6369015a231
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-25 06:27:07 +0700

    Publish stable HRD renderer via internal-reports-loader.js

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -65,7 +65,7 @@
     started=true;
 
     if(isHrd()){
-      load('hrd-role-cleanup.js?v=20260925-promo11');
+      load('hrd-role-cleanup.js?v=20260925-stable12');
       return true;
     }
     if(r==='kabid_tahfizh')return true;
