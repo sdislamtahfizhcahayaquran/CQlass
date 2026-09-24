766e2cc5929047b2e003db71b1a713cfd19a489f
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-25 06:49:06 +0700

    Publish Promo numbering via internal-reports-loader.js

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -65,7 +65,7 @@
     started=true;
 
     if(isHrd()){
-      load('hrd-role-cleanup.js?v=20260925-stable12');
+      load('hrd-role-cleanup.js?v=20260925-stable13');
       return true;
     }
     if(r==='kabid_tahfizh')return true;
