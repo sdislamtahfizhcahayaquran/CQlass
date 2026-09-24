50e4d0031ac8705cc75af70fc57f6da285a81491
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-25 06:10:14 +0700

    Publish polished HRD Promo Socmed via internal-reports-loader.js

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -65,7 +65,7 @@
     started=true;
 
     if(isHrd()){
-      load('hrd-role-cleanup.js?v=20260925-isolate10');
+      load('hrd-role-cleanup.js?v=20260925-promo11');
       return true;
     }
     if(r==='kabid_tahfizh')return true;
