d58fa72c394f97ac3af78300147fc22945ae9702
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-25 01:51:08 +0700

    Align HRD lazy loader with isolated workspace

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -65,7 +65,7 @@
     started=true;
 
     if(isHrd()){
-      load('hrd-role-cleanup.js?v=20260924-hrd-clean2');
+      load('hrd-role-cleanup.js?v=20260925-hrd-isolated4');
       return true;
     }
     if(r==='kabid_tahfizh')return true;
