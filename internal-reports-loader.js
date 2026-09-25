605e2acf1b4a350c5c733c46227987b7ef2ddcd7
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-25 08:28:18 +0700

    Publish audited HRD monthly filters via internal-reports-loader.js

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -65,7 +65,7 @@
     started=true;
 
     if(isHrd()){
-      load('hrd-role-cleanup.js?v=20260925-stable15');
+      load('hrd-role-cleanup.js?v=20260925-stable16');
       return true;
     }
     if(r==='kabid_tahfizh')return true;
