d292f66cbff73eb478d23791431cabb03ed5bcd2
sdislamtahfizhcahayaquran <sditcqdepok@gmail.com>
2026-09-25 07:11:05 +0700

    Publish Promo evidence gallery via internal-reports-loader.js

--- internal-reports-loader.js
+++ internal-reports-loader.js
@@ -65,7 +65,7 @@
     started=true;
 
     if(isHrd()){
-      load('hrd-role-cleanup.js?v=20260925-stable13');
+      load('hrd-role-cleanup.js?v=20260925-stable14');
       return true;
     }
     if(r==='kabid_tahfizh')return true;
